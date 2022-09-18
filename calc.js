f83ecc302fdae86cbdb36bb4b3a10415ac25e865
Irfan <isk22802@gmail.com>
2022-09-18 23:03:52 +0530

    colors,font size increased

--- calc.js
+++ calc.js
@@ -1,5 +1,6 @@
 var text=document.getElementById("display");
 //text.innerHTML="WELCOME";
+text.style.fontSize="40px";
 var but=Array.from(document.getElementsByClassName('button'));
 but.map( button => {
     button.addEventListener('click',(e) => {
@@ -7,7 +8,7 @@ but.map( button => {
             case 'AC':
                 text.innerHTML='';
                 break;
-            case 'del':
+            case 'DEL':
                 text.innerHTML=text.innerHTML.slice(0,-1);    
                 break; 
             case '=':
